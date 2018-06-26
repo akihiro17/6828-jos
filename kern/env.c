74b1046b418c62567a753415f64a04477f3babc9
akihiro17 <coolwizard11@gmail.com>
2018-06-26 18:34:52 +0900

    lab3 ~ex8

--- kern/env.c
+++ kern/env.c
@@ -24,7 +24,7 @@ static struct Env *env_free_list;	// Free environment list
 // Set up global descriptor table (GDT) with separate segments for
 // kernel mode and user mode.  Segments serve many purposes on the x86.
 // We don't use any of their memory-mapping capabilities, but we need
-// them to switch privilege levels. 
+// them to switch privilege levels.
 //
 // The kernel and user segments are identical except for the DPL.
 // To load the SS register, the CPL must equal the DPL.  Thus,
@@ -518,9 +518,8 @@ env_run(struct Env *e)
           curenv->env_status = ENV_RUNNING;
         }
         curenv = e;
-        curenv->env_status = ENV_RUNNING;
-        curenv->env_runs++;
-        lcr3(PADDR(curenv->env_pgdir));
-        env_pop_tf(&curenv->env_tf);
+        e->env_status = ENV_RUNNING;
+        e->env_runs++;
+        lcr3(PADDR(e->env_pgdir));
+        env_pop_tf(&e->env_tf);
 }
-
